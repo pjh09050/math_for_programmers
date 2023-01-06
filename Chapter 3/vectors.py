e1b9a695f19db1a131fc70250258a2d925bc1c19
Park Jinhwan <106535998+pjh09050@users.noreply.github.com>
2023-01-06 16:53:57 +0900

    review

--- Chapter 3/vectors.py
+++ Chapter 3/vectors.py
@@ -1,16 +1,26 @@
-from math import sqrt, sin, cos, atan2
+from math import sqrt, sin, cos, acos, atan2
 
 # def add(v1,v2):
 #     return (v1[0] + v2[0], v1[1] + v2[1])
 
-def subtract(v1,v2):
-    return (v1[0] - v2[0], v1[1] - v2[1])
+
+
+# def add(*vectors):
+#     by_coordinate = zip(*vectors)
+#     coordinate_sums = [sum(coords) for coords in by_coordinate]
+#     return tuple(coordinate_sums)
 
 def add(*vectors):
-    return (sum([v[0] for v in vectors]), sum([v[1] for v in vectors]))
+    return tuple(map(sum,zip(*vectors)))
+    
+def subtract(v1,v2):
+    return tuple(v1-v2 for (v1,v2) in zip(v1,v2))
 
 def length(v):
-    return sqrt(v[0]**2 + v[1]**2)
+    return sqrt(sum([coord ** 2 for coord in v]))
+
+def dot(u,v):
+    return sum([coord1 * coord2 for coord1,coord2 in zip(u,v)])
 
 def distance(v1,v2):
     return length(subtract(v1,v2))
@@ -21,7 +31,7 @@ def perimeter(vectors):
     return sum(distances)
 
 def scale(scalar,v):
-    return (scalar * v[0], scalar * v[1])
+    return tuple(scalar * coord for coord in v)
 
 def to_cartesian(polar_vector):
     length, angle = polar_vector[0], polar_vector[1]
@@ -38,3 +48,20 @@ def to_polar(vector):
     x, y = vector[0], vector[1]
     angle = atan2(y,x)
     return (length(vector), angle)
+
+def angle_between(v1,v2):
+    return acos(
+                dot(v1,v2) /
+                (length(v1) * length(v2))
+            )
+
+def cross(u, v):
+    ux,uy,uz = u
+    vx,vy,vz = v
+    return (uy*vz - uz*vy, uz*vx - ux*vz, ux*vy - uy*vx)
+
+def component(v,direction):
+    return (dot(v,direction) / length(direction))
+
+def unit(v):
+    return scale(1./length(v), v)
